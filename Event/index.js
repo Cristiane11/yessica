50b77fcb8462c403d295aef0f52e12fb73917c81
Cristiane Abreu <bjwmcd@yahoo.com>
2025-06-20 09:46:37 -0400

    update

---
+++ Event/index.js
@@ -0,0 +1,62 @@
+const projects =[
+    {
+        title:'react',
+        description:'A wordpress website to a local broker',
+        technologies:["HTML", "CSS", "JavaScript", "Wordpress", "php"],
+        image:'./#',
+        link:'./google.com'
+    },
+     {
+        title:'bootstrap',
+        description:'A wordpress website to a local broker',
+        technologies:["React", "CSS", "JavaScript", "Wordpress", "php"],
+        image:'./#',
+        link:'./google.com'
+    },
+     {
+        title:'flexbox',
+        description:'A wordpress website to a local broker',
+        technologies:["Bootstrap", "CSS", "JavaScript", "Wordpress", "php"],
+        image:'./#',
+        link:'./google.com'
+    },
+     {
+        title:'Medical Supplies',
+        description:'A wordpress website to a local broker',
+        technologies:["flexbox", "CSS", "JavaScript", "Wordpress", "php"],
+        image:'./#',
+        link:'./google.com'
+    }
+]
+const container = document.getElementById('project-container');
+const dropDown = document.getElementById('filter-select');
+const sortBtn = document.getElementById('sort-btn')
+
+//
+projects.forEach(project =>{
+    const card = document.createElement('div');
+    card.className = "project-card";
+    card.innerHTML = `
+    <img src="${project.image}" alt="${project.title}">
+    <h2>${project.title}</h2>
+    <p>${project.description}</p>
+    <p><strong>Tech:</strong> ${project.technologies.join(", ")}</p>
+    <a href="${project.link}" target="_blank">View Project</a>
+    `;
+    container.appendChild(card)
+});
+dropDown.addEventListener('change', (event)=>{
+    const value = event.target.value;
+    let filtered = [];
+    if (value === 'all'){
+        filtered = projects;
+    }else if (value === 'bootstrap'){
+        filtered = projects.filter((proj)=>proj.title ="bootstrap") 
+    }else if (value === 'flexbox' ){
+        filtered = projects.filter((proj)=>proj.title ="flexbox") 
+    }else if(value === 'react'){
+      filtered = projects.filter((proj)=>proj.title ="react")   
+    }
+  sortBtn.onclick = container(filtered);
+
+});
