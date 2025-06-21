021230c7bcfaac3782a8861a8a021fcf4a45216a
Cristiane Abreu <bjwmcd@yahoo.com>
2025-06-21 17:15:17 -0400

    update

--- Event/index.js
+++ Event/index.js
@@ -32,56 +32,47 @@ const container = document.getElementById('project-container');
 const dropDown = document.getElementById('filter-select');
 const sortBtn = document.getElementById('sort-btn')
 
+let currentProjects =[...projects];
+// Function to display projects
+function displayProjects(list){
+container.innerHTML = '';//clear container
+ list.forEach(project =>{
+    
+    const card = document.createElement('div');
+    card.className = "project-card";
+    card.innerHTML = `
+    <img src="${project.image}" alt="${project.title}">
+    <h2>${project.title}</h2>
+    <p>${project.description}</p>
+    <p><strong>Tech:</strong> ${project.technologies.join(", ")}</p>
+    <h2>${project.title}</h2>
+    <p>${project.description}</p>
+    <p><strong>Tech:</strong> ${project.technologies.join(", ")}</p>
+    <a href="${project.link}" target="_blank">View Project</a>
+    `;
+    container.appendChild(card);
+    });
+ }
+    displayProjects(currentProjects);
 
-projects.forEach(project =>{
-    if(project.technologies.includes('Bootstrap')){
-     const cardBootstrap = document.createElement('div');
-    card.className = "project-card";
-    card.innerHTML = `
-    <img src="${project.image}" alt="${project.title}">
-    <h2>${project.title}</h2>
-    <p>${project.description}</p>
-    <p><strong>Tech:</strong> ${project.technologies.join(", ")}</p>
-    <a href="${project.link}" target="_blank">View Project</a>
-    `;
-    container.appendChild(cardBootstrap)   
-    }else if(project.technologies.includes('React')){
-      
-    const cardReact = document.createElement('div');
-    card.className = "project-card";
-    card.innerHTML = `
-    <img src="${project.image}" alt="${project.title}">
-    <h2>${project.title}</h2>
-    <p>${project.description}</p>
-    <p><strong>Tech:</strong> ${project.technologies.join(", ")}</p>
-    <a href="${project.link}" target="_blank">View Project</a>
-    `;
-    container.appendChild(cardReact)   
-    }else if (project.technologies.includes('flexbox')){
-    const cardFlexbox= document.createElement('div');
-    card.className = "project-card";
-    card.innerHTML = `
-    <img src="${project.image}" alt="${project.title}">
-    <h2>${project.title}</h2>
-    <p>${project.description}</p>
-    <p><strong>Tech:</strong> ${project.technologies.join(", ")}</p>
-    <a href="${project.link}" target="_blank">View Project</a>
-    `;
-    container.appendChild(cardFlexbox)   
-    }
+    dropDown.addEventListener('change', (event) => {
+  const value = event.target.value;
+  if (value === 'all') {
+    currentProjects = [...projects];
+  } else if (value === 'bootstrap'){
+    currentProjects = projects.technologies.includes("bootstrap");
+  }else if (value === 'flexbox'){
+    currentProjects = projects.technologies.includes("flexbox");
+  }else if (value === 'react'){
+    currentProjects = projects.technologies.includes("react");
+  }
+  
+  displayProjects(currentProjects);
 });
-dropDown.addEventListener('change', (event)=>{
-    const value = event.target.value;
-    let filtered = [];
-    if (value === 'all'){
-        filtered = projects;
-    }else if (value === 'bootstrap'){
-        filtered = cardBootstrap;
-    }else if (value === 'flexbox' ){
-        filtered = cardFlexox;
-    }else if(value === 'react'){
-      filtered = cardReact;   
-    }
-  sortBtn.onclick = container(filtered);
 
+// Sort by title
+sortBtn.addEventListener('click', () => {
+  currentProjects.sort((a, b) => a.title.localeCompare(b.title));
+  displayProjects(currentProjects);
 });
+
